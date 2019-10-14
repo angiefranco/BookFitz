8feecf84056d81ba937e3cf18251981ceca40ae9
Angie Franco <afranco4@nd.edu>
2019-10-14 12:58:44 -0400

    update options dropdown

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-// /*Dropdown Menu*/
-// $('.dropdown').click(function () {
-//         $(this).attr('tabindex', 1).focus();
-//         $(this).toggleClass('active');
-//         $(this).find('.dropdown-menu').slideToggle(300);
-//     });
-//     $('.dropdown').focusout(function () {
-//         $(this).removeClass('active');
-//         $(this).find('.dropdown-menu').slideUp(300);
-//     });
-//     $('.dropdown .dropdown-menu li').click(function () {
-//         $(this).parents('.dropdown').find('span').text($(this).text());
-//         $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
-//     });
-// /*End Dropdown Menu*/
+/*Dropdown Menu*/
+$('.dropdown').click(function () {
+        $(this).attr('tabindex', 1).focus();
+        $(this).toggleClass('active');
+        $(this).find('.dropdown-menu').slideToggle(300);
+    });
+    $('.dropdown').focusout(function () {
+        $(this).removeClass('active');
+        $(this).find('.dropdown-menu').slideUp(300);
+    });
+    $('.dropdown .dropdown-menu li').click(function () {
+        $(this).parents('.dropdown').find('span').text($(this).text());
+        $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+    });
+/*End Dropdown Menu*/
 //
 //
 // $('.dropdown-menu li').click(function () {
@@ -20,7 +20,7 @@
 //       msg = '<span class="msg">Hidden input value: ';
 //   $('.msg').html(msg + input + '</span>');
 // });
-background = square.getStyle('background');
-  $$('.custom-dropdown select').each(function (dropdown) {
-    dropdown.setStyle({'background' : background});
-  });
+// background = square.getStyle('background');
+//   $$('.custom-dropdown select').each(function (dropdown) {
+//     dropdown.setStyle({'background' : background});
+//   });
